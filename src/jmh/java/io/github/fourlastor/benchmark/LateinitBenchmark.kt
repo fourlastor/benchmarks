5562eb8debaa6d873954bf166853705025329673
Daniele Conti <fourlastor@users.noreply.github.com>
2022-11-11 16:53:11 +0000

    Add lateinit benchmarks

---
+++ src/jmh/java/io/github/fourlastor/benchmark/LateinitBenchmark.kt
@@ -0,0 +1,47 @@
+package io.github.fourlastor.benchmark
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import java.util.concurrent.*
+
+@Warmup(iterations = 7, time = 1)
+@Measurement(iterations = 7, time = 1)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+@Fork(2)
+open class LateinitBenchmark {
+
+    lateinit var notInitialized: Any
+    lateinit var initialized: Any
+
+    var nullField: Any? = null
+    var nonNullField: Any? = 1
+
+    @Setup
+    fun init() {
+        initialized = 1
+    }
+
+    @Benchmark
+    fun checkNotInitialized(blackhole: Blackhole) {
+        blackhole.consume(this::notInitialized::isInitialized)
+    }
+
+    @Benchmark
+    fun checkInitialized(blackhole: Blackhole) {
+        blackhole.consume(this::initialized::isInitialized)
+    }
+
+    @Benchmark
+    fun checkNoNull(blackhole: Blackhole) {
+        blackhole.consume(nullField == null)
+    }
+
+    @Benchmark
+    fun checkNull(blackhole: Blackhole) {
+        blackhole.consume(nonNullField == null)
+    }
+}
